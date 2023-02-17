fa5b7fc031a4d78edcf9cb939b0161cc7f144ade
Reubsafram <reubenafram@gmail.com>
2023-02-16 19:25:19 -0800

    line

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - this time use printf option
  * Return: 0 (Correct)
  */
+
 int main(void)
+
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }	
