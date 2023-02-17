fae916f359a292f0f3741bb85866c8711630b5ed
Reubsafram <reubenafram@gmail.com>
2023-02-16 20:45:22 -0800

    ghdhfd

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,4 @@
 #include <stdio.h>
-/**
- * main - this time use printf
- * Return:always 0.
- */
-int main(void)
-{#include <stdio.h>
 
 /**
  * main - Entry point
@@ -15,7 +9,4 @@ int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
 }	
