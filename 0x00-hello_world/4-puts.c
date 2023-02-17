8ba86ea476e4ef7159cce6755705797c9ae0174b
Reubsafram <reubenafram@gmail.com>
2023-02-16 17:08:17 -0800

    betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C program using puts function
+ * Return: 0 (Correct)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
