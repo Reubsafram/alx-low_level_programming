43d816cf5c79379dc3c3074b81f2e9f1afdfbc0d
Reubsafram <reubenafram@gmail.com>
2023-02-16 20:39:03 -0800

    neew

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
+/**
+ * main - this time use printf
+ * Return:always 0.
+ */
+int main(void)
+{#include <stdio.h>
 
 /**
- * main - this time use printf option
- * Return: Always 0.
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+}
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }	
